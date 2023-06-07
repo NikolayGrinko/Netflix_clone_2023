44e8949a9bd129305976d88823bd084521b77fda
grinya37 <112849355+grinya37@users.noreply.github.com>
2023-06-08 02:41:44 +0300

    add_project

---
+++ NetflixClone/ViewModels/TitlePreviewViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TitlePreviewViewModel.swift
+//  NetflixClone
+//
+//  Created by Николай Гринько on 24.05.2023.
+//
+
+import Foundation
+
+struct TitlePreviewViewModel {
+    let title: String
+    let youtubeView: VideoElement
+    let titleOverview: String
+}
